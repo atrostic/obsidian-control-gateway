86fa4b2aa379755e4de6a615a1737f9f74a178ce
atrostic <atrostic@gmail.com>
2026-04-07 13:28:36 -0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -39,8 +39,6 @@ export async function onRequest(context) {
     return next();
   }
 
-  /** NOTHING TO SEE HERE **/
-
   const cookies = parseCookies(request.headers.get("Cookie") || "");
 
   for (const requiredCookie of requiredCookies) {
